5e655bc6e18f08ee348e35cf8d0964cb00a55dec
Natalia-Barragan <natalia.barragannb@gmail.com>
2026-01-19 17:05:30 -0300

    arreglo minimo

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,13 +29,13 @@ export function Footer() {
             >
               <Instagram size={18} />
             </a>
-            <a
+            {/* <a
               href="mailto:coni.perez.arte@gmail.com"
               className="w-10 h-10 border border-primary-foreground/30 flex items-center justify-center hover:bg-primary-foreground hover:text-primary transition-colors"
               aria-label="Email"
             >
               <Mail size={18} />
-            </a>
+            </a> */}
           </div>
         </div>
 
